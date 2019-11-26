723f8d2e478e490f6ad07cd51e7cd3ac962be071
Nobunaga Taira <e195753@ie.u-ryukyu.ac.jp>
2019-11-26 09:59:35 +0900

    third code

--- src/main/ExceptionTest.java
+++ src/main/ExceptionTest.java
@@ -1,11 +1,6 @@
 public class ExceptionTest {
     public static void main(String[] args){
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("ぬるぽ");
-            System.out.println(e.getMessage());
-        }
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
     }
 }
